eb2f46fc99ebeb8f3f9bcb80812ca61fe30c8457
Lauren <147095403+BuggInTheWeb@users.noreply.github.com>
2025-11-08 11:05:37 +0000

    rest of practice test

--- solutions.js
+++ solutions.js
@@ -33,3 +33,79 @@ value = value.toString();
 /* boolean expression that uses all three 
 logical operators (AND, OR, NOT). */
 
+
+let isAdult = true;
+let hasTicket = false;
+let withParent = true;
+
+let accessGranted = (isAdult && hasTicket) || (isAdult && !hasTicket && withParent);
+
+console.log(accessGranted); 
+
+/* function that takes single nr as parameter and returns boolean
+depending on if input is + or - */
+
+function checkPositive(n) {
+    return n > 0;
+}
+
+console.log(checkPositive(-3));
+
+/* write 2 loops, a 'for' and a 'while' which each 
+output nrs 1 thru 10 inclusive */
+
+//for loop
+let i = 1;
+
+for(i = 1; i <= 10; i++) {
+    alert(i);
+}
+
+//while loop
+i = 1;
+while (i < 11) {
+  alert(i);
+  i++;
+}
+
+/*ask user for nr then using single if, output msg
+if input is either 5, 13 or divisible by 3 */
+
+    let inputNr = prompt("Enter any number", '');
+    n = Number(inputNr);
+
+
+    if(n === 5 || n === 13 || n % 3 === 0) {
+    alert("your number is either 5, 13 or divisible by 3");
+    }
+
+/*Prompt for fav season. Use chained if...else if 
+to output diffr msg depending on input. 
+Now write alt way to solve using switch instead. */
+let favSeason = prompt("What's your fav season?", "");
+
+if(favSeason == "summer") {
+    alert("sun's out!");
+} else if(favSeason == "autumn") {
+    alert("crunchy leaves time");
+} else if(favSeason == "winter") {
+    alert("the cozy indoor season");
+} else if(favSeason == "spring") {
+    alert("the season in full bloom");
+}
+
+
+let favoritSeason = prompt("What season do you prefer?", "");
+
+switch (favoritSeason) {
+    case "summer":
+        alert("Sun's Out!!");
+    case "autumn":
+        alert("Crunchy leaf time");
+    case "winter":
+        alert("cozy time indoors");
+    case "spring":
+        alert("a blooming good time");
+}
+
+
